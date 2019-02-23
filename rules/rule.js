3c32e93e43cc9a8ef602de47554bd040eedaa1d7
Juan Carlos Medina <juanca.med@gmail.com>
2019-02-22 18:48:33 -0800

    Implement import rule
    Ensures import expressions use __real__ paths instead of SASS-like paths.
    Ensures import expressions always declare an extension.
    
    These two rules ensures each import expression is CSS compatible.

--- rules/rule.js
+++ rules/rule.js
@@ -1,3 +1,60 @@
-module.exports = {};
+const fs = require('fs');
+const path = require('path');
+const stylelint = require('stylelint');
+
+const messages = stylelint.utils.ruleMessages('import/rule', {
+  expected: (filePath) => `Unable to find ${filePath}`,
+});
+
+
+module.exports = stylelint.createPlugin('css-modules/composed-class-names', (primaryOption, secondaryOptionObject) => {
+  const options = secondaryOptionObject || { resolve: undefined };
+  const resolve = options.resolve || { alias: {}, modules: [] };
+  const aliasMap = resolve.alias || {};
+  const aliases = Object.keys(aliasMap);
+  const modules = resolve.modules || [];
+
+  function resolveFilePath(contextPath, filePath) {
+    if (filePath[0] === '.') {
+      return path.join(contextPath, filePath);
+    } else {
+      // Copy webpack resolution algorithm -- is this a module I can just import?
+      let resolvedFilePath = filePath;
+      const rootDirectory = resolvedFilePath.split(path.sep)[0];
+      const rootAlias = aliases.find(alias => alias === rootDirectory);
+
+      if (rootAlias) {
+        resolvedFilePath = path.join(aliasMap[rootAlias], ...resolvedFilePath.split(path.sep).slice(1))
+        if (fs.existsSync(resolvedFilePath)) {
+          return resolvedFilePath;
+        }
+      }
+
+      resolvedFilePath = modules
+        .map(module => path.join(module, resolvedFilePath))
+        .find(maybePath => fs.existsSync(maybePath));
+
+      return resolvedFilePath || filePath;
+    }
+  }
+
+
+  return (root, result) => {
+    const contextPath = path.dirname(result.opts.from);
+
+    root.walkAtRules('import', (atRule) => {
+      const filePath = resolveFilePath(contextPath, atRule.params.slice(1, -1));
+
+      if (!fs.existsSync(filePath)) {
+        stylelint.utils.report({
+          message: messages.expected(filePath),
+          node: atRule,
+          result: result,
+          ruleName: 'import/rule',
+        });
+      }
+    });
+  };
+});
 
 module.exports.ruleName = 'import/rule';
--- rules/rule.test.js
+++ rules/rule.test.js
@@ -1,5 +1,49 @@
 const rule = require('./rule.js');
+const stylelint = require('stylelint');
+
+function configuration(options) {
+  return Object.assign({
+    config: {
+      plugins: ['./rule.js'],
+      rules: {
+        'import/rule': [true, {}],
+      },
+    },
+    configBasedir: 'rules',
+  }, options);
+}
 
 test('it has a name', () => {
   expect(rule.ruleName).toBe('import/rule');
+});
+
+test('rule parses correct import statements', () => {
+  return stylelint.lint(configuration({
+    files: require.resolve('../fixtures/passes.css'),
+  })).then(function (resultObject) {
+    const output = JSON.parse(resultObject.output);
+    expect(output[0].warnings).toEqual([]);
+  });
+});
+
+test('rule errors on missing files', () => {
+  return stylelint.lint(configuration({
+    files: require.resolve('../fixtures/fails-missing-file.css'),
+  })).then(function (resultObject) {
+    expect(resultObject.errored).toBe(true);
+
+    const output = JSON.parse(resultObject.output);
+    expect(output[0].warnings[0].severity).toBe('error');
+  });
+});
+
+test('rule errors on missing extensions', () => {
+  return stylelint.lint(configuration({
+    files: require.resolve('../fixtures/fails-missing-extension.css'),
+  })).then(function (resultObject) {
+    expect(resultObject.errored).toBe(true);
+
+    const output = JSON.parse(resultObject.output);
+    expect(output[0].warnings[0].severity).toBe('error');
+  });
 });
